1e3606a0068b17ad7474061100dbbc007950b9a8
Hamzah Qazi <h.qazi@ion2s.local>
2019-05-21 17:00:25 +0200

    Added sorted function

--- DupArr.js
+++ DupArr.js
@@ -1,5 +1,38 @@
 let theArray = [123, 22, 123, 22, 11, 22, 11, 3, 1, 2, 22];
 
+/* ich gebe durch das array
+ich wähle jedes einzelne Element aus 
+ich suche das kleinste Element
+und stelle es an am anfang des Arrays */
+
+function bubbleSort(array) {
+    let swapped
+    let n = theArray.length;
+
+    do {
+        swapped = false;
+        for (let i = 0; i < n; i++) {
+            if (array[i] > array[i+1]) {
+                const temporary = array[i];
+                /* console.log('eins: ',temporary); */
+                array[i] = array[i+1];
+                /* console.log('zwei',array[i+1]); */
+                array[i+1] = temporary;
+                /* console.log('drei', array[i+1]); */
+                swapped = true;
+            }
+        }
+    }
+
+    while(swapped)
+
+    return array;
+    
+}
+
+let sorted = bubbleSort(theArray);
+console.log(sorted);
+
 /* schleife durchläuft das Array über jedes Element
 prüfen ob das Element an den verschiedenen Index vorhanden ist
     ist das Element 
@@ -8,31 +41,13 @@ ist dies nicht der Fall wird die Überprüfung verlassen und auf das nächste El
  */
 
 
-/* const duplicateArray = [];
-theArray.forEach((searchItem) => {
-    var count = 0;
-    theArray.forEach((duplicateCheckItem) => {
-        
-        if (searchItem === duplicateCheckItem) {
-            console.log(duplicateCheckItem);
-            count++;
-        }
-            
-        if(count > 1){
-            duplicateArray.push(searchItem);
-            console.log('Ein guter Text: ', duplicateArray);
-        }
 
-    })
-}); */
-
-const duplicateArray = [];
+const duplicateArray = []; 
 
 theArray.forEach((searchItem) => {
     var count = 0;
     theArray.forEach(function(duplicateCheckItem) { 
         if (searchItem === duplicateCheckItem) {
-            /* console.log(duplicateCheckItem); */
             count++;
         }
     })
@@ -46,33 +61,17 @@ theArray.forEach((searchItem) => {
         if (!isContaining) {
             duplicateArray.push(searchItem);
         }  
+
+        function sortIt(dupArray) {
+            let swapped;
+            for (let i = 0; i < dupArray.length; i++) {
+                const element = array[i];
+                
+            }
+            while(swapped)
+        }
     }
     
 });
 console.log('Ein guter Text: ', duplicateArray);
-
-
-
-
-
-
-/* let dupArray = [123];
-const finalArray = [];
-let cnt = null; 
-
-theArray.forEach((a1)=>{
-    console.log('Zahlen von theArray: ' + a1)
-    dupArray.forEach((a2)=>{
-        console.log('Zahlen von dupArray: ' + a2);
-        if(a1 === a2) {
-            finalArray.push(a2);
-        }
-    });
-    dupArray.push(a1);
-});
-
-
  
-console.log(dupArray);
-console.log(finalArray);
- */
