bb4350d06b24f3d9b67012d7c8cee874d50824ae
Hamzah Qazi <h.qazi@ion2s.local>
2019-05-21 18:00:12 +0200

    Clean Code Style and add Palindrom function

--- DupArr.js
+++ DupArr.js
@@ -1,19 +1,35 @@
-let theArray = [123, 22, 123, 22, 11, 22, 11, 3, 1, 2, 22];
+/* 
+    Hier erstelle ich ein Palindromchecker das Wort wird in den Parameter 'str'
+    eingeben und die for Schleife durchläuft dann das Wort
+    da das Word dekremntiert wird, wird der letze Buchstabe genommen und
+    in die Varaibale reversed eingetragen
+*/
 
-/* ich gebe durch das array
-ich wähle jedes einzelne Element aus 
-ich suche das kleinste Element
-und stelle es an am anfang des Arrays */
+function reverse(str) {
+    let reversed = '';
+    for(let i = str.length-1; i >= 0; i--){
+        reversed += str[i];
+        console.log(reversed);
+    }
+    if(reversed === str) {
+        console.log(reversed + ' word is a palindrome');
+    }else{
+        console.log(reversed + ' word is not a plaindrom');
+    }
+ 
+    return reversed;
+}
 
+reverse('word');
 
-/* schleife durchläuft das Array über jedes Element
-prüfen ob das Element an den verschiedenen Index vorhanden ist
+/* 
+    schleife durchläuft das Array über jedes Element
+    prüfen ob das Element an den verschiedenen Index vorhanden ist
     ist das Element 
-wenn dies der Fall ist dann sollen die Element in ein neues Element gepusht werden 
-ist dies nicht der Fall wird die Überprüfung verlassen und auf das nächste Element gesetzt
+    wenn dies der Fall ist dann sollen die Element in ein neues Element gepusht werden 
+    ist dies nicht der Fall wird die Überprüfung verlassen und auf das nächste Element gesetzt
  */
-
-
+let theArray = [123, 22, 123, 22, 11, 22, 11, 3, 1, 2, 22];
 
 const duplicateArray = []; 
 
@@ -35,25 +51,34 @@ theArray.forEach((searchItem) => {
             duplicateArray.push(searchItem);
         }  
 
-        function sortIt(dupArray) {
-            let swapped;
-            do {
-                swapped = false;
-                for (let i = 0; i < dupArray.length; i++) {
-                    if(dupArray[i] > dupArray[i+1]) {
-                        const element = array[i];
-                        array[i] = array[i+1];
-                        array[i+1] = element;
-                        swapped = true;
-                    }
-                }
+    }
+    /* console.log('Ein guter Text: ', sortIt(duplicateArray)); */
+});
+
+
+/* 
+    ich gebe durch das array
+    ich wähle jedes einzelne Element aus    
+    ich suche das kleinste Element
+    und stelle es an am anfang des Arrays 
+*/
+
+function sortIt(dupArray) {
+    let swapped;
+    do {
+        swapped = false;
+        for (let i = 0; i < dupArray.length; i++) {
+            if(dupArray[i] > dupArray[i+1]) {
+                const element = dupArray[i];
+                dupArray[i] = dupArray[i+1];
+                dupArray[i+1] = element;
+                swapped = true;
             }
-            while(swapped)
-            return dupArray;
         }
     }
-    
-});
+    while(swapped)
+    return dupArray;
+}
 /* console.log('Ein guter Text: ', duplicateArray); */
 console.log('Ein guter Text: ', sortIt(duplicateArray));
  
