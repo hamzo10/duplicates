863c8ddc008e7a2e71a36a8e7729769e57c22984
Hamzah Qazi <h.qazi@ion2s.local>
2019-05-24 17:48:29 +0200

    Create new functions

--- createClass.js
+++ createClass.js
@@ -1,68 +1,85 @@
-function Name (nameArray) {
-    this.name = ['H', 'A', 'M', 'Z', 'A', 'H'];
+
+let nameArr = [];
+function Name (charac) {
+    this.name = charac;
 }
+var name = new Name(nameArr); 
 
-/* function nameArray () {
-    const name = ['H', 'A', 'M', 'Z', 'A', 'H'];
-    return name;
-} */
+/* 
+Convert String into an Array
+*/
 
-let arr1 = [1, 2, 3, 4, 5];
-let arr2 = [6, 7, 8, 9];
+let character = 'Hamzah';
 
-function concat (arr1, arr2) {
-    let glued = arr1 + arr2;
-    return glued;
-}
-
-console.log(arr1, arr2);
-
-
-
-
-// inc = function () {
-//     if(!this.counter){
-//         this.counter = 0
-//     }
-//     this.counter ++;
-//     console.log(this.counter)
-// }
-
-
-
-
-/* Hero.prototype.inc = function () {
-    if(Hero.prototype.counter=== undefined){
-        Hero.prototype.counter = 0
+function names(name) {
+    for (let char = 0; char < name.length; char++) {
+        nameArr.push(name[char]);
     }
-    Hero.prototype.counter ++;
-    return Hero.prototype.counter
-    
+    /* console.log(nameArr); */
+    return nameArr;
 }
+
+console.log(names(character));
+
+console.log(name);
+
+/* 
+    ToString Method
+*/
+
+function toString (letter) {
+    let str = '';
+        nameArr.forEach((i) => {
+            str += i;
+            // if(index != (nameArr + 1)) {
+            //     str += ',';
+            // };
+        });
+    return str;
+}
+ console.log(toString(nameArr));
+
+
+ /* 
+    Delete String
  */
 
+ function deleteString (arr) {
+    for (let letter = 0; letter < nameArr.length; letter++) {
+        let start = nameArr[i];
+        let stop = nameArr[i];
+        if() {
 
-/* console.log(hero1);
-hero1.greet() */
+        }
 
-// function Warrior (name, level, weapon) {
-//     Hero.call(this, name, level);
-//     this.weapon = weapon;
+    }
+ }
+ console.log(deleteString(nameArr));
+
+ /*
+    Lenght of an Array
+ */
+
+ function getLength() {
+     for () {
+
+     }
+ }
+ 
+
+// let arr1 = [1, 2, 3, 4, 5];
+// let arr2 = [6, 7, 8, 9, 'Hallo'];
+//let arr3 = [];
+// for (let i = 0; i < arr1.length; i++) {
+//     arr3.push(arr1[i]);
+//     /* console.log(arr3); */
+// }
+// for (let j = 0; j < arr2.length; j++) {
+//     arr3.push(arr2[j])
+//     /* console.log(arr3); */
 // }
 
-// function Healer (name, level, spell) {
-//     Hero.call(this, name, level);
-//     this.spell = spell;
-// }
 
-// Warrior.prototype.attack = function () {
-//     return `${this.name} attacks with ${this.weapon}`;
-// }
-
-// Healer.prototype.heal = function () {
-//     return `${this.name} casts ${this.spell}`;
-// }
-
-// const hero1 = new Warrior('Bjorn', 1, 'axe');
-// const hero2 = new Healer ('Jack', 1, 'cure');
+// console.log(arr3);
+// console.log(arr3[9] + ' ich bin Hamzah ich bin ' + arr1[1] + arr1[2] + ' Jahre alt');
 
